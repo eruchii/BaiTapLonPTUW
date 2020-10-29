87e87472d4432aa0d3d3b65b6efb3a498e1c1660
honest113 <httstartteh@gmail.com>
2020-10-29 11:14:34 +0700

    form login/ regis

--- easyaccomod/routes.py
+++ easyaccomod/routes.py
@@ -1,5 +1,7 @@
+from flask.helpers import url_for
+from easyaccomod.forms import LoginForm, RegistrationForm
 from easyaccomod import app
-from flask import render_template
+from flask import render_template, redirect
 
 @app.route('/')
 @app.route('/home')
@@ -9,3 +11,20 @@ def home():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # tao form nhung chua xu ly dang ky
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        return redirect(url_for('login'))
+    return render_template("register.html", form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # tao form nhung chua xu ly dang nhap
+    form = LoginForm()
+    if form.validate_on_submit():
+        # xu ly dang nhap tai day
+        return redirect(url_for("home"))
+    return render_template("login.html", form=form)
