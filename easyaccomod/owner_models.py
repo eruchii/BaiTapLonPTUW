8d81a923b425a782dabde7911b4ca625a9c12552
eruchii <nct1120@gmail.com>
2020-11-06 09:17:15 +0700

    Update owner_models.py

--- easyaccomod/owner_models.py
+++ easyaccomod/owner_models.py
@@ -18,9 +18,9 @@ class Room(db.Model):
 	__tablename__ = 'room_pending'
 	id = db.Column(db.Integer, primary_key=True)
 	owner_id = db.Column(db.Integer, db.ForeignKey('owner.id')) #lien ket owner
-	city_code = db.Column(db.String(120), db.ForeignKey('city.code')) #lien ket voi bang city
-	district_id = db.Column(db.Integer, db.ForeignKey('district.id')) #lien ket voi district
-	ward_id = db.Column(db.Integer, db.ForeignKey('ward.id')) #lien ket voi ward
+	city_code = db.Column(db.String(5), db.ForeignKey('city.code')) #lien ket voi bang city
+	district_id = db.Column(db.String(10), db.ForeignKey('district.id')) #lien ket voi district
+	ward_id = db.Column(db.String(10), db.ForeignKey('ward.id')) #lien ket voi ward
 	info = db.Column(db.String(120))
 	room_type_id = db.Column(db.Integer)
 	room_number = db.Column(db.Integer)
@@ -35,29 +35,29 @@ class Room(db.Model):
 	gia_dien = db.Column(db.Integer)
 	gia_nuoc = db.Column(db.Integer)
 	tien_ich_khac = db.Column(db.Text)
-
 	image = db.Column(db.Text)
+	pending = db.Colum(db.Boolean)
 
 	def __repr__(self):
 		return "<Room(id='{}', owner_id='{}'>".format(self.id, self.owner_id)
 
 class City(db.Model):
-	code = db.Column(db.String(120), primary_key=True)
+	code = db.Column(db.String(5), primary_key=True)
 	name = db.Column(db.String(120))
 	def __repr__(self):
 		return "<City(code='{}', name='{}'>".format(self.code, self.name) 
 
 class District(db.Model):
-	city_code = db.Column(db.String(120), db.ForeignKey('city.code'))
-	id = db.Column(db.String(120), primary_key=True)
+	city_code = db.Column(db.String(5), db.ForeignKey('city.code'))
+	id = db.Column(db.String(10), primary_key=True)
 	name = db.Column(db.String(120))
 	def __repr__(self):
 		return "<District(city.code='{}', id='{}', name='{}'>".format(self.city_code, self.id, self.name)
 
 class Ward(db.Model):
-	city_code = db.Column(db.String(120), db.ForeignKey('city.code'))
-	district_id = db.Column(db.String(120), db.ForeignKey('district.id'))
-	id = db.Column(db.String(120), primary_key=True)
+	city_code = db.Column(db.String(5), db.ForeignKey('city.code'))
+	district_id = db.Column(db.String(10), db.ForeignKey('district.id'))
+	id = db.Column(db.String(10), primary_key=True)
 	name = db.Column(db.String(120))
 	def __repr__(self):
 		return "<Ward(city.code='{}', district.id='{}', id='{}', name='{}'>".format(self.city_code, self.district_id, self.id, self.name)
