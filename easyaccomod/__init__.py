11e150f3134234bc633af48862fa72b44c76bb48
honest113 <httstartteh@gmail.com>
2020-11-06 08:44:19 +0700

    sqlite - db

--- easyaccomod/__init__.py
+++ easyaccomod/__init__.py
@@ -7,8 +7,8 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '80e63eb7f7af4b8bb32edc7839994cb2'
 # mysql+pymysql://root:''@localhost/easyaccomod
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/easyaccomod' # test
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/easyaccomod' # test
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
