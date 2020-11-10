359a228b029186d5ceeadb7b2c72b41509b81f3f
eruchii <nct1120@gmail.com>
2020-11-10 10:14:45 +0700

    aaaa

--- easyaccomod/owner_models.py
+++ easyaccomod/owner_models.py
@@ -8,14 +8,19 @@ class Owner(db.Model):
 	fullname = db.Column(db.String(120))
 	identity_number = db.Column(db.String(120))
 	phone_number = db.Column(db.String(120))
+	email = db.Column(db.String(120), unique = True)
+	city_code = db.Column(db.String(5), db.ForeignKey('city.code'))
+	district_id = db.Column(db.String(10), db.ForeignKey('district.id'))
+	ward_id = db.Column(db.String(10), db.ForeignKey('ward.id'))
 	status = db.Column(db.Integer, db.ForeignKey('confirm.id'))
+
+	rooms = db.relationship("Room", backref="owner")
 	def __repr__(self):
 		return "<Owner(username='%s', password='%s', fullname='%s', identity_number='%s', phone_number='%s', status='%s'>" % (
 			self.username, self.password, self.fullname, self.identity_number, self.phone_number, self.status
 			)
 
 class Room(db.Model):
-	__tablename__ = 'room_pending'
 	id = db.Column(db.Integer, primary_key=True)
 	owner_id = db.Column(db.Integer, db.ForeignKey('owner.id')) #lien ket owner
 	city_code = db.Column(db.String(5), db.ForeignKey('city.code')) #lien ket voi bang city
@@ -44,6 +49,8 @@ class Room(db.Model):
 class City(db.Model):
 	code = db.Column(db.String(5), primary_key=True)
 	name = db.Column(db.String(120))
+	owners = db.relationship("Owner", backref="city", lazy=True)
+	rooms = db.relationship("Room", backref="city", lazy=True)
 	def __repr__(self):
 		return "<City(code='{}', name='{}'>".format(self.code, self.name) 
 
@@ -51,6 +58,8 @@ class District(db.Model):
 	city_code = db.Column(db.String(5), db.ForeignKey('city.code'))
 	id = db.Column(db.String(10), primary_key=True)
 	name = db.Column(db.String(120))
+	owners = db.relationship("Owner", backref="district", lazy=True)
+	rooms = db.relationship("Room", backref="district", lazy=True)
 	def __repr__(self):
 		return "<District(city.code='{}', id='{}', name='{}'>".format(self.city_code, self.id, self.name)
 
@@ -59,5 +68,7 @@ class Ward(db.Model):
 	district_id = db.Column(db.String(10), db.ForeignKey('district.id'))
 	id = db.Column(db.String(10), primary_key=True)
 	name = db.Column(db.String(120))
+	owners = db.relationship("Owner", backref="ward", lazy=True)
+	rooms = db.relationship("Room", backref="ward", lazy=True)
 	def __repr__(self):
 		return "<Ward(city.code='{}', district.id='{}', id='{}', name='{}'>".format(self.city_code, self.district_id, self.id, self.name)
