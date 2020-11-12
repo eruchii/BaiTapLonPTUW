45f35c9ed568c543b8a1fbb5e320a2486702350d
nglong153 <42497344+nglong153@users.noreply.github.com>
2020-11-12 10:47:07 +0700

    Reconstruct  room search module

--- easyaccomod/renter_views.py
+++ easyaccomod/renter_views.py
@@ -1,37 +1,25 @@
 from flask import render_template, redirect, url_for, flash, request, abort,jsonify, make_response
 from flask_login import login_user, current_user, logout_user, login_required
-from easyaccomod.owner_models import City,District,Ward
 from easyaccomod import app
 from easyaccomod.forms import SearchForm
-
-
+from easyaccomod.renter_routes import getDistrict, getStreet, getResult,getCity
 
 @app.route("/renter/search",methods = ['POST','GET'])
 @login_required
 def search():
-    cities = City.query.all()
+    cities = getCity()
     form = SearchForm(cities) 
-    print(request)
+    #print(request)
     if (request.method == "POST"):
         req = request.get_json()
-
         if (req[0] == 'city'):
-          cityCode = City.query.filter_by(name = req[1]).first().code
-          
-          district = District.query.filter_by(city_code = cityCode).all()
-          ret =['']
-          for _obj in district:
-            ret.append(_obj.name)
-          res = make_response(jsonify(ret),200)       
-          return res
+          return getDistrict(req[1])
         
         elif (req[0] == 'district'):
-          districtCode = District.query.filter_by(name=req[1]).first().id
-          streetList = Ward.query.filter_by(district_id = districtCode).all()
-          ret = ['']
-          for _obj in streetList:
-            ret.append(_obj.name)
-          res = make_response(jsonify(ret),200)
+          return getStreet(req[1])
+        elif (req[0] == 'submit'):
+          return getResult(req[1])
+        else:
+          res = make_response(jsonify("loi roi dit me may request cai buoi`"),200)
           return res
-    
     return render_template("searchRoom.html",form = form)
