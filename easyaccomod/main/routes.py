8c9a9540349f1e379c110c0da0e7e1bbf7310d31
nglong153 <42497344+nglong153@users.noreply.github.com>
2020-12-24 14:49:53 +0700

    Redirect Url

--- easyaccomod/main/routes.py
+++ easyaccomod/main/routes.py
@@ -1,12 +1,15 @@
 from flask import Blueprint
 from flask import render_template
-
+from easyaccomod.forms import SearchForm
+from easyaccomod.owner_models import City
 main = Blueprint('main', __name__)
 
 @main.route('/')
 @main.route('/home')
 def home():
-    return render_template("home.html")
+    cities = City.query.all()
+    form = SearchForm(cities)
+    return render_template("/renterFrontPage.html",form = form)
 
 @main.route('/about')
 def about():
